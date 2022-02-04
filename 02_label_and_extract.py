7ad892168abc28643bbc8b4ea9843b30ddb3bd6c
macyapp <sidhantn99@gmail.com>
2022-02-04 07:43:59 +0530

    Face Feature Extraction v2

--- 02_label_and_extract.py
+++ 02_label_and_extract.py
@@ -66,17 +66,23 @@ for (i, rect) in enumerate(rects):
 		# specific face part
 		for (x, y) in shape[i:j]:
 			cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
-        # extract the ROI of the face region as a separate image
+		# extract the ROI of the face region as a separate image
 		(x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
 		roi = image[y:y + h, x:x + w]
 		roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
 		# show the particular face part
 		cv2.imshow("ROI", roi)
 		cv2.imshow("Image", clone)
+		if cv2.waitKey(1):
+			if 0xFF == ord('e'):
+				continue
+			if 0xFF == ord('q'):
+				continue
 		cv2.waitKey(0)
 	# visualize all facial landmarks with a transparent overlay
 	output = face_utils.visualize_facial_landmarks(image, shape)
 	cv2.imshow("Image", output)
 	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
 
