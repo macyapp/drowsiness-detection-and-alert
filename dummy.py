296f05d080dd77bd2495b4f29a50a0e04470d3a4
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-05 13:05:58 +0530

    ud

--- dummy.py
+++ dummy.py
@@ -1,4 +1,17 @@
-	# apply the transparent overlay
-	cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
-	# return the output image
-	return output
+		# extract the ROI of the face region as a separate image
+		(x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+		roi = image[y:y + h, x:x + w]
+		roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+		# show the particular face part
+		cv2.imshow("ROI", roi)
+		cv2.imshow("Image", clone)
+        
+        key = cv2.waitKey(1) & 0xFF
+ 		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+		cv2.waitKey(0)
+	# visualize all facial landmarks with a transparent overlay
+	output = face_utils.visualize_facial_landmarks(image, shape)
+	cv2.imshow("Image", output)
+	cv2.waitKey(0)
